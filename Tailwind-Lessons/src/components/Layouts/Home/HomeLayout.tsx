e0b2249a606422adb179ce5ac8436ac2587409dd
nicsylvia <nicsylvia15f@gmail.com>
2023-05-17 07:37:17 -0700

    frontend architecture

--- Tailwind-Lessons/src/components/Layouts/Home/HomeLayout.tsx
+++ Tailwind-Lessons/src/components/Layouts/Home/HomeLayout.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Footer, Header } from "../../Static";
+import { Outlet } from "react-router-dom";
 
 const HomeLayout = () => {
-  return <div>HomeLayout</div>;
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default HomeLayout;
