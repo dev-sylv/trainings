0d9ebb08076ef52b730d4fa7c33d128bb9b2bf75
nicsylvia <nicsylvia15f@gmail.com>
2023-05-17 06:34:30 -0700

    gotten all users

--- CRUD/src/controller/userController.ts
+++ CRUD/src/controller/userController.ts
@@ -1,4 +1,19 @@
 import userModel from "../model/userModel";
 import { Request, Response } from "express";
 
-export const getAllUsers = async () => {};
+//get all users
+export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await userModel.find();
+
+    return res.status(200).json({
+      message: "gotten",
+      data: users,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: "failed to get all users",
+      data: error,
+    });
+  }
+};
